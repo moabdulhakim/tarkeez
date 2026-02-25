87299df7ba4a4311d1a430f1efbd76966cfc4798
Mohammad Abdulhakim <duxwork0@gmail.com>
2026-02-26 01:35:04 +0200

    ci: configure GitHub Actions workflow for cross-platform builds

--- src/main/java/com/example/tarkeez/TarkeezApp.java
+++ src/main/java/com/example/tarkeez/TarkeezApp.java
@@ -3,6 +3,7 @@ package com.example.tarkeez;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,6 +15,10 @@ public class TarkeezApp extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(TarkeezApp.class.getResource("view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm()) ;
+
+        Image logo = new Image(getClass().getResourceAsStream("/images/icon.png"));
+        stage.getIcons().add(logo);
+
         stage.setTitle("Tarkeez: Focus and Write notes!");
         stage.setScene(scene);
         stage.setMaximized(true);
